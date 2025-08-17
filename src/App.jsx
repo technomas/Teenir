a9da7d6fd83a94a76f1d664c84d3c6c2b6732513
techsave <technomas221@gmail.com>
2025-08-17 07:33:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -577,7 +577,7 @@ export default function App() {
 
           {/* Payment Methods */}
           <div>
-            <h2 className="text-2xl font-bold mb-4 text-yellow-500">PAYMENTS</h2>
+            <h2 className="text-2xl font-bold mb-4 text-red-500">PAYMENTS</h2>
             <div className="flex flex-row gap-4 justify-center md:justify-start">
               {/* Litecoin */}
               <a href="https://imgbb.com/" target="_blank" rel="noopener noreferrer">
