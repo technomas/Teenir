1ba83e175fad70dbd9f3081a0d855eccda75cdff
techsave <technomas221@gmail.com>
2025-09-05 03:39:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -634,7 +634,6 @@ export default function App() {
                   <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0极狐 z" clipRule="evenodd" />
                 </svg>
               </button>
-            </div>
           </section>
         </main>
 
